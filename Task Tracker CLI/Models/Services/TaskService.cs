fafb201237c50d31656d361772737846b0758286
loaqdev <ponchik22893@gmail.com>
2026-02-16 13:16:40 +0100

    Добавьте файлы проекта.

---
+++ Task Tracker CLI/Models/Services/TaskService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_Tracker_CLI.Models.Services;
+
+internal class TaskService
+{
+}
---
+++ Task Tracker CLI/Models/TaskModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_Tracker_CLI.Models;
+
+internal class TaskModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Description { get; set; } = string.Empty;
+
+    public enum TaskStatus
+    {
+        todo,
+        inProgress,
+        done
+    }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+}
